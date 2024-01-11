dc7814a6cd8dc78b129874421c9b19e9bfd83f0f
robel tewelde <robeltewalde29@gmail.com>
2024-01-11 09:58:46 +0300

    add

--- registration/script.js
+++ registration/script.js
@@ -1,24 +1,25 @@
 const age = 23;
 const p = `Hey you are ${age} years old`;
-const entry = document.getElementById("entry");
 
-const nam = document.getElementById("nam");
+const form = document.getElementById("Registration");
+const nam1 = document.getElementById("nam");
 const dep = document.getElementById("dep");
 const pay = document.getElementById("pay");
 const select = document.getElementById("select");
 const class1 = document.getElementById("class");
+const entry = document.getElementById("entry");
 
 const submit = document.getElementById("submit");
 
 submit.addEventListener("click", (event) => {
-  event.preventDefault();
+  // event.preventDefault();
 
   const nam1 = nam.value;
   const dep1 = dep.value;
-  const pay1 = pay.value;
-  const select1 = select.value;
+  const pay = pay.value;
+  const select = select.value;
   const class1 = class1.value;
-  console.log(dep1);
+  console.log(nam1);
 
   const p = document.createElement("p")
   p.textContent(`Hey ${nam1}!! \n you choose ${dep1} to study and agreed to pay `)
