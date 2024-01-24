6eaa9e0651d52c67378c89ec22cfba4f25251fe7
robeltewelde <149680946+robeltewelde@users.noreply.github.com>
2024-01-24 15:43:12 +0300

    add

--- registration2/script.js
+++ registration2/script.js
@@ -1,3 +1,4 @@
+let selectedRaw = null;
 const nam1 = document.getElementById("nam");
 const dep = document.getElementById("department");
 const pey = document.getElementById("payment");
@@ -10,35 +11,39 @@ const tBody = document.getElementById("Body");
 
 submit.addEventListener("click", (event) => {
   event.preventDefault();
+  if (selectedRaw == null) {
+    const nam1 = nam.value;
+    const dep1 = dep.value;
+    const pey1 = pey.value;
+    const sch1 = sch.value;
+    const shift1 = shift.value;
+    const class2 = class1.value;
+    const data = [nam1, dep1, pey1, sch1, shift1, class2];
 
-  const nam1 = nam.value;
-  const dep1 = dep.value;
-  const pey1 = pey.value;
-  const sch1 = sch.value;
-  const shift1 = shift.value;
-  const class2 = class1.value;
-  const data = [nam1, dep1, pey1, sch1, shift1, class2];
-
-  for (let i = 0; i <= data.length; i++) {
-    if (data[i] === "") {
-      alert("Please enter", data[i]);
-      break;
-    } else {
-      const raw = tableRaw([nam1, dep1, pey1, sch1, shift1, class2]);
-      tBody.appendChild(raw);
-      clearForm();
-      break;
+    for (let i = 0; i <= data.length; i++) {
+      if (data[i] === "") {
+        alert("Please enter", data[i]);
+        break;
+      } else {
+        const raw = tableRaw([nam1, dep1, pey1, sch1, shift1, class2]);
+        tBody.appendChild(raw);
+        clearForm();
+        break;
+      }
     }
+  } else {
+    updateRaw();
   }
 });
 
 function clearForm() {
   nam.value = "";
-  dep.value = "";
+  dep.value = dep[0].value;
   pey.value = "";
-  sch.value = "";
-  shift.value = "";
-  class1.value = "";
+  sch.value = sch[0].value;
+  shift.value = shift[0].value;
+  class1.value = class1[0].value;
+  selectedRaw = null;
 }
 
 function tableRaw(data) {
@@ -86,20 +91,33 @@ tBody.addEventListener("click", (e) => {
     tr_del.remove();
   }
   if (element.textContent == "Edit") {
-    const tr_edit = element.parentElement.parentElement;
+    selectedRaw = element.parentElement.parentElement;
 
-    console.log(tr_edit.getElementsByTagName("td")[0]);
+    // console.log(selectedRaw.getElementsByTagName("td")[1].textContent);
     document.getElementById("nam").value =
-      tr_edit.getElementsByTagName("td")[0].textContent;
-      document.getElementById("department").value =
-      tr_edit.getElementsByTagName("td")[0].textContent;
-      document.getElementById("payment").value =
-      tr_edit.getElementsByTagName("td")[0].textContent;
-      document.getElementById("sch").value =
-      tr_edit.getElementsByTagName("td")[0].textContent;
-      document.getElementById("shift").value =
-      tr_edit.getElementsByTagName("td")[0].textContent;
-      document.getElementById("class").value =
-      tr_edit.getElementsByTagName("td")[0].textContent;
+      selectedRaw.getElementsByTagName("td")[0].textContent;
+    document.getElementById("department").value =
+      selectedRaw.getElementsByTagName("td")[1].textContent;
+    document.getElementById("payment").value =
+      selectedRaw.getElementsByTagName("td")[2].textContent;
+    document.getElementById("sch").value =
+      selectedRaw.getElementsByTagName("td")[3].textContent;
+    document.getElementById("shift").value =
+      selectedRaw.getElementsByTagName("td")[4].textContent;
+    document.getElementById("class").value =
+      selectedRaw.getElementsByTagName("td")[5].textContent;
   }
 });
+
+function updateRaw() {
+  const inputFeilds = document.querySelectorAll(
+    "#nam1, #dep, #pey, #sch, #shift, #class1"
+  );
+  selectedRaw.cells[0].textContent = nam1.value;
+  selectedRaw.cells[1].textContent = dep.value;
+  selectedRaw.cells[2].textContent = pey.value;
+  selectedRaw.cells[3].textContent = sch.value;
+  selectedRaw.cells[4].textContent = shift.value;
+  selectedRaw.cells[5].textContent = class1.value;
+  clearForm();
+}
